a6c14baad307042f1d5e4ae61dde682cb692022c
Huang <mail@huanghong.top>
2022-04-25 11:21:06 +0800

    add actuator request trace page
    Signed-off-by: Huang <mail@huanghong.top>

--- src/router/routerMap.js
+++ src/router/routerMap.js
@@ -70,8 +70,18 @@ export const routerMap = [
                     title: '监控',
                     icon: 'meh'
                 },
-                redirect: '/monitor/system',
+                redirect: '/monitor/httpTrace',
                 children: [
+                    {
+                        path: '/monitor/httpTrace',
+                        name: 'HttpTrace',
+                        component: (resolve) => require(['@/views/monitor/HttpTrace'], resolve),
+                        meta: {
+                            title: '请求追踪',
+                            icon: 'meh',
+                            hideHead: false
+                        }
+                    },
                     {
                         path: '/monitor/system',
                         name: 'System',
@@ -131,57 +141,6 @@ export const routerMap = [
                             title: 'Disk监控',
                             icon: 'meh',
                             hideHead: false
-                        }
-                    }
-                ]
-            },
-            {
-                path: '/page1',
-                name: 'Page1',
-                component: BlankLayout,
-                meta: {
-                    title: '页面一',
-                    icon: 'meh'
-                },
-                redirect: '/page1/page2',
-                children: [
-                    {
-                        path: '/page1/page2',
-                        name: 'Page2',
-                        component: (resolve) => require(['@/views/dashboard/Page1'], resolve),
-                        meta: {
-                            title: '页面二',
-                            icon: 'meh'
-                        }
-                    },
-                    {
-                        path: '/page1/page5',
-                        name: 'Page5',
-                        component: (resolve) => require(['@/views/dashboard/Page1'], resolve),
-                        meta: {
-                            title: '页面五',
-                            icon: 'meh'
-                        }
-                    }
-                ]
-            },
-            {
-                path: '/page3',
-                name: 'Page3',
-                component: BlankLayout,
-                meta: {
-                    title: '页面三',
-                    icon: 'meh'
-                },
-                redirect: '/page3/page4',
-                children: [
-                    {
-                        path: '/page3/page4',
-                        name: 'Page4',
-                        component: (resolve) => require(['@/views/dashboard/Page2'], resolve),
-                        meta: {
-                            title: '页面四',
-                            icon: 'meh'
                         }
                     }
                 ]
