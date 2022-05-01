7b1131b70dc4ba7e56430e657a8b5ba51baf4b47
Huang <mail@huanghong.top>
2022-05-01 20:13:08 +0800

    user avatar update
    Signed-off-by: Huang <mail@huanghong.top>

---
+++ src/store/module/user.js
@@ -0,0 +1,34 @@
+import {getCurrentUser} from "@/api/user";
+
+const state = {
+  info: {}
+}
+
+const mutations = {
+  SET_USER: (state,user) => {
+    state.info = user
+  }
+}
+
+const actions = {
+  setUser({ commit },user) {
+    commit('SET_USER',user)
+  },
+  GetInfo({ commit }) {
+    return new Promise((resolve, reject) => {
+      getCurrentUser().then(res => {
+        commit('SET_USER', res.user)
+        resolve(res)
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
