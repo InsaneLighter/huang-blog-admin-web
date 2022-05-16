87e6afbc907322cf3296f6ed675f0de7fb989bbf
Huang <mail@huanghong.top>
2022-05-16 19:00:15 +0800

    add message module
    Signed-off-by: Huang <mail@huanghong.top>

---
+++ src/api/message/index.js
@@ -0,0 +1,25 @@
+import {service, transformGetData} from '@/utils/request'
+
+export function page(data) {
+    let params = transformGetData(data);
+    return service({
+        url: 'api/admin/message' + params,
+        method: 'get'
+    })
+}
+
+export function del(ids) {
+    if (typeof ids === "string") {
+        ids = Array.of(ids);
+    }
+    return service({
+        url: 'api/admin/message',
+        method: 'delete',
+        data: ids
+    })
+}
+
+export default {
+    page,
+    del
+}
